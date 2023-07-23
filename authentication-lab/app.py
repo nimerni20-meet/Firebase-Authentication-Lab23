530f6a298de651997dd69403d01759f7b64fbbcf
nimerni20-meet <139688587+nimerni20-meet@users.noreply.github.com>
2023-07-23 16:32:20 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -23,3 +23,39 @@ def add_tweet():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+firebaseConfig = {
+  'apiKey': "AIzaSyAvEeJXjtPejmnIphkox18ts4kDAIvQpQ4",
+ ' authDomain': "lab-c4556.firebaseapp.com",
+  'projectId': "lab-c4556",
+  'storageBucket': "lab-c4556.appspot.com",
+  'messagingSenderId': "980248028540",
+  'appId': "1:980248028540:web:197a93e8e7e72f00f670f9",
+  'measurementId': "G-KR93543B28",
+  "databaseURL":""
+};
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+
+
+
+app.route('/signup', methods=['GET', 'POST'])
+def signup():
+   error = ""
+   if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+        user = login_session['user']
+       try:
+            login_session['user'] = 
+auth.create_user_with_email_and_password(email, password)
+           return redirect(url_for('home'))
+       except:
+           error = "Authentication failed"
+   return render_template("signup.html")
+
+
+
+
